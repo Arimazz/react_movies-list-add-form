8cb9448d0ffcd17f635c3366567068473f5e8a70
Arimazz <bodyaprymyak@icloud.com>
2020-02-21 18:13:54 +0200

    Updates

---
+++ precache-manifest.4ae60f1a09d647f1bc87796560f76503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4915d2c3ba40f42b0c0dbb87b9aa1c8",
+    "url": "/react_movies-list-add-form/index.html"
+  },
+  {
+    "revision": "e1c02f6efb50a0a551d5",
+    "url": "/react_movies-list-add-form/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "b6860378a0a70d151fd4",
+    "url": "/react_movies-list-add-form/static/css/main.ad9fc1a7.chunk.css"
+  },
+  {
+    "revision": "e1c02f6efb50a0a551d5",
+    "url": "/react_movies-list-add-form/static/js/2.97e219aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_movies-list-add-form/static/js/2.97e219aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6860378a0a70d151fd4",
+    "url": "/react_movies-list-add-form/static/js/main.f1f0bce4.chunk.js"
+  },
+  {
+    "revision": "38a5643bbe04a47a847e",
+    "url": "/react_movies-list-add-form/static/js/runtime-main.ae2b7a9d.js"
+  }
+]);
